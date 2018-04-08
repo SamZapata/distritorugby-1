53883f6b0a4a881c3d5237c2ef9fd82608fce36a
SamZapata <legionario.zapata@gmail.com>
2018-04-07 20:54:27 -0500

    Correción sobre cloudinary. Gema para almacenar las imagenes como CDN

--- app/models/history.rb
+++ app/models/history.rb
@@ -6,7 +6,7 @@ class History < ActiveRecord::Base
 	:storage => :cloudinary,
 	:path => ':id/:style/:filename',
 	:cloudinary_credentials => Rails.root.join("config/cloudinary.yml")
-	
-	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/"
-  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	#has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/"
+  	
 end
